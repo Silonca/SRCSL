bee6efe0daca1dcf8b98c0a259f854bb0c93f069
Silonca <729240629@qq.com>
2019-11-17 00:26:49 +0800

    add a macro version

--- Recorder/recoder.h
+++ Recorder/recoder.h
@@ -6,4 +6,22 @@
 void Recoder(void *data,uint32_t length, uint8_t newdata);
 
 
+
+#define RECODER(TYPE,LENGTH) \
+struct RECODER_TYPE\
+{\
+    (TYPE) data[LENGTH];\
+}
+
+
+#define RECODER_FUNC(TYPE,LENGTH,NEWDATA)\
+Recoder_TYPE Recoder_TYPE_Func( RECODER(TYPE,LENGTH) *DATA, TYPE NEWDATA)\
+{\
+    for( uint32_t a = 0; a < LENGTH - 1; ++a)\
+    {\
+        DATA[a + 1] = DATA[a];\
+    }\
+    DATA[0] = NEWDATA;\
+}
+
 #endif
