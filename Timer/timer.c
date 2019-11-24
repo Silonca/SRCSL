ce11595196d68559aab5d51bb90fd3bf808f826a
Silonca <729240629@qq.com>
2019-11-25 00:16:17 +0800

    act orderly

--- Timer/timer.c
+++ Timer/timer.c
@@ -1,6 +1,10 @@
 #include "timer.h"
 
+#include <stdlib.h>
+
 static volatile uint32_t timer_cnt = 0;
+
+enum { SET, RESET};
 
 void Timer_Updata()
 {
@@ -19,3 +23,142 @@ uint32_t Timer_Get( Timer *timer)
     return timer_cnt - timer->time;
 }
 
+
+
+//------------------------------------------------------------
+
+void Act_Init( Act_Orderly *ao, char *list, void ( *act_func)( uint8_t action_code))
+{
+    ao->list = list;
+
+    ao->pause_flag = SET;
+    ao->timer_start_flag = RESET;
+    ao->processing_index = 0;
+    ao->processing_action = 0;
+    ao->processing_time = 0;
+
+    ao->act_func = act_func;
+}
+
+
+void Act_Start( Act_Orderly *ao)
+{
+    ao->pause_flag = RESET;
+    ao->timer_start_flag = SET;
+}
+
+
+void Act_Pause( Act_Orderly *ao)
+{
+    ao->pause_flag = SET;
+}
+
+
+void Act_Stop( Act_Orderly *ao)
+{
+    ao->pause_flag = SET;
+    ao->processing_index = 0;
+}
+
+
+void Act_Restart( Act_Orderly *ao)
+{
+    ao->processing_index = 0;
+    ao->pause_flag = RESET;
+    ao->timer_start_flag = SET;
+}
+
+
+void Act_Jump_Seq( Act_Orderly *ao, uint8_t seq)
+{
+    Act_Restart( ao);
+    for( uint8_t a = 0; a < seq; ++a)
+    {
+        Act_List_Analyse( ao);
+        ++ao->processing_index;
+    }
+}
+
+
+void Act_Jump_Time( Act_Orderly *ao, uint32_t time)
+{
+    uint32_t time_sum = 0;
+    Act_Restart( ao);
+    do
+    {
+        
+        Act_List_Analyse( ao);
+        ++ao->processing_index;
+        time_sum += ao->processing_time;
+    }
+    while( time_sum < time);
+}
+
+uint8_t Act_List_Analyse( Act_Orderly *ao)
+{
+    //get the next action from the string
+    ao->processing_action = atoi( ao->list + ao->list_process);
+    //handling malformed
+    if( ao->processing_action == -1 || ao->processing_action == 0)
+    {
+        Act_Stop( ao);
+        return -1;
+    }
+
+    //find the "," and next pending subscript index
+    for( ++ao->list_process; ao->list[ ao->list_process] != ','; ++ao->list_process)
+    {
+        if( ao->list[ ao->list_process] != '\0')
+        {
+            Act_Stop( ao);
+            return -1;
+        }
+    }
+
+    //get the next latency from the string
+    ao->processing_time = atoi( ao->list + ao->list_process);
+    //handling malformed input
+    if( ao->processing_time == -1 || ao->processing_time == 0)
+    {
+        Act_Stop( ao);
+        return -1;
+    }
+
+    //find the ";" and next pending subscript index
+    for( ++ao->list_process; ao->list[ ao->list_process] != ';'; ++ao->list_process)
+    {
+        if( ao->list[ ao->list_process] != '\0')
+        {
+            Act_Stop( ao);
+            return -1;            
+        }
+
+    }
+    return 0;
+}
+
+void Act_Server( Act_Orderly *ao)
+{
+    if( ao->pause_flag == RESET)
+    {
+        if( ao->timer_start_flag == SET)
+        {
+            Timer_Set( &( ao->timer));
+            ao->timer_start_flag = RESET;
+        }
+
+        if( Timer_Get( &( ao->timer)) % ao->processing_time == 0)
+        {
+            if( Act_List_Analyse( ao) == -1)
+            {
+                //some code to handle the malformed input
+            }
+            ao->act_func( ao->processing_action);
+        }
+    }
+    else 
+    {
+        ++ao->processing_index;
+    }
+    
+}
--- Timer/timer.h
+++ Timer/timer.h
@@ -14,4 +14,37 @@ void Timer_Set( Timer *timer);                                          //Run th
 uint32_t Timer_Get( Timer *timer);                                      //Run this function to get the time from Timer_Set() till now
 
 
+//----------------------------------------------------------
+
+typedef struct
+{
+    char *list;                                 //Frame format: action_code(1~255),latency(1~2^32)
+    uint16_t list_process;
+
+    uint8_t pause_flag;
+    uint8_t timer_start_flag;
+    uint8_t processing_index;                   //It seems to be useless
+    uint8_t processing_action;
+    uint32_t processing_time;
+
+    Timer timer;
+
+    void ( *act_func)(uint8_t action_code);     //Must be a non-blocking function
+} Act_Orderly;
+
+
+void Act_Init( Act_Orderly *ao, char *list, void ( *act_func)( uint8_t action_code));
+
+void Act_Start( Act_Orderly *ao);                       //Start and Continue
+void Act_Pause( Act_Orderly *ao);                       //Pause
+void Act_Stop( Act_Orderly *ao);                        //Stop
+void Act_Restart( Act_Orderly *ao);                     //Restart
+
+void Act_Jump_Seq( Act_Orderly *ao, uint8_t seq);       //Jump to a specified frame
+void Act_Jump_Time( Act_Orderly *ao, uint32_t time);    //Jump to the frame after the given time
+
+void Act_Server( Act_Orderly *ao);                      //Server Function, must run periodically
+
+
+
 #endif
